562020f389aa2a7aa540e99d99c178083df096e1
ankitlpu1008 <49818622+ankitlpu1008@users.noreply.github.com>
2019-04-23 21:50:05 +0530

    Add files via upload

---
+++ third.c
@@ -0,0 +1,44 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+#include<unistd.h>
+struct A{
+	char name[10];
+	int qt;
+};
+void Bill(struct A a[],int g)
+{
+float price;
+int i,j;
+printf("\n\n Do you want print Bill??");
+printf("\n\n\n\t Enter Y for'YES'  or N for 'NO'::-");
+scanf("%s",&j);
+if(j=='Y'){
+printf("\n\n\tEnter price of one gift::-");
+scanf("%f",&price);
+printf("\n\n\t\t\t||Bill of students in order are||");
+printf("\n\n\t\tName\t\tQuantity\t\tPrice\n");
+for(i=0;i<g;i++)
+{
+	printf("\n\n\t\t%s\t\t%d\t\t\t%.2f",a[i].name,a[i].qt,(a[i].qt)*price);
+}
+printf("\n\n\tThank you..\n\n Have a great Day\n");
+}
+else
+printf("\n\n\tThank you..\n");
+}
+void program()
+{
+	int p;
+	int j,i;
+	printf("\n\t\t\tEnter No.of students::-");
+	scanf("%d",&p);
+	struct A r[p],temp;
+	printf("\nEnter Name of students*");
+	for(i=0;i<p;i++)
+	{
+		printf("\n\n\t\tEnter Number %d Name:-",i+1);
+		scanf("%s",r[i].name);
+		printf("\n\n\t\tEnter Number %d Quantity of Gifts:-",i+1);
+		scanf("%d",&r[i].qt);
+	}
